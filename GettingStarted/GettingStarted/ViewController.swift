e4630deff886bd621151932ae1f9cd22444dc3c8
AADI KRISHNA <aadikrishna04@gmail.com>
2023-08-24 08:49:37 -0500

    finished Whack A Mole

---
+++ GettingStarted/GettingStarted/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  GettingStarted
+//
+//  Created by Aadi Krishna on 8/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Aadi Krishna on 8/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
