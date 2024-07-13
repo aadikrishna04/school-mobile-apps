85711f16b9c9a3a2688d121285597a8d9f98638e
AADI KRISHNA <aadikrishna04@gmail.com>
2024-07-13 13:18:12 -0400

    finished school year

--- Anagrams/Anagrams/AnagramsView.swift
+++ Anagrams/Anagrams/AnagramsView.swift
@@ -8,8 +8,12 @@
 import SwiftUI
 
 struct AnagramsView: View {
+    @State var rootWord = ""
+    @State var wordsList = [""]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Hello")
+        }
     }
 }
 
